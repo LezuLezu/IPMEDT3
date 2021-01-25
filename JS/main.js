ca1d3af5a107fa9cc1742c7d0d41f4e87e4b1c91
Zoë <>
2021-01-25 15:23:07 +0100

    Revert "Merge branch 'whiteboard' into dev"
    This reverts commit 257ada55fce110a40f317ab4f811692cc9be61e9, reversing
    changes made to 24d61601a630416160251b9b0de091f4f551575d.

--- JS/main.js
+++ JS/main.js
@@ -77,29 +77,5 @@ window.onload = () => {
         }
       });
     }
-    // Whiteboard
-
-    const testDoos = document.getElementById('js--doos');
-    const display = document.getElementById('js--displayBox');
-    const inhoud = ["Hallo welkom \n bij onze \n VR practicum", "Item2", "Item3"];
-    const afvalBak = document.getElementById('js--afvalBak');
-    let index = 0;
-
-    function laatTextZien(getal) {
-      console.log(getal);
-      display.setAttribute("value", inhoud[getal]);
-    }
-
-
-    testDoos.onclick = (event) => {
-      console.log(index);
-      setTimeout(() => {
-        laatTextZien(index), index++, afvalBak.setAttribute("opacity", 1);
-      }, 2000);
-
-
-      responsiveVoice.speak(inhoud[index], "Dutch Male");
-
-    }
 
 }
