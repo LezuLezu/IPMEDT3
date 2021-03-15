f6a68ba7ccc7ff84c2fc3a04c1fc1b60b8942eaa
MarZwa <mgm.zwaan@gmail.com>
2021-03-15 12:37:08 +0100

    Api toegevoegd

--- JS/reloader.js
+++ JS/reloader.js
@@ -1,22 +1,20 @@
-AFRAME.registerComponent("planten", {
+AFRAME.registerComponent("dieren", {
     init: function() {
         // only works for a-images with pokemon attribute/component
-        const planeten = document.querySelectorAll("[planeet]");
-        const BASE_URL = "https://swapi.dev/api/planets/";
-        const KEY = '?token=a7c8-yYT-Rzc6uh9a1riX4yjfhHNr4coHSIhvetef3s';
-        this.newPlaneet = function() {
-            for(let i=0; i < planeten.length; i++) {
-                let randomNum = Math.floor(Math.random() * 59) + 1;
-                fetch(BASE_URL + randomNum)
+        const dieren = document.querySelectorAll("[dier]");
+        const BASE_URL = "https://dog.ceo/api/breeds/image/random";
+        // const BASE_URL_END = ".jpg"
+        this.newDier = function() {
+            for(let i=0; i < dieren.length; i++) {
+                // let randomNum = Math.floor(Math.random() * 122) + 1;
+                fetch(BASE_URL)
                     .then(response => response.json())
-                    .then(data => planeten[i].setAttribute("value", ("Planet name: " + data.name + "\n Climate: " + data.climate + "\n Terrain: " + data.terrain)));
+                    .then(data => dieren[i].setAttribute("src", data.message));
             }
         }
-
-        this.el.addEventListener("mouseenter", this.newPlaneet);
     },
     update: function() {
-        this.newPlaneet();
+        this.newDier();
     },
     tick: function() {},
     remove: function() {},
